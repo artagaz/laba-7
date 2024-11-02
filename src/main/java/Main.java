6d849e1cf1b017ba4476bfef46cd499402b2ee64
Andrew <and.vaylin@gmail.com>
2024-11-02 21:34:51 +0700

    5+6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,17 @@
 import com.google.gson.Gson;
+import lombok.Builder;
 import lombok.Data;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.*;
 import java.util.*;
-
-import static java.util.Comparator.comparing;
 
 public class Main {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
         //parsing JSON
         File file = new File("/home/andrew/IdeaProjects/laba-7/books.json");
-        Reader reader = null;
+        Reader reader;
         try {
             reader = new FileReader(file);
         } catch (FileNotFoundException e) {
@@ -24,35 +21,79 @@ public class Main {
 
         Person[] person = gson.fromJson(reader, Person[].class);
 
-        //1
-        ArrayList<String> visitors = new ArrayList<>();
-        for (Person p : person){
-            visitors.add(p.getName()+" "+p.getSurname());
+        int Select = 0;
+        try {
+            Select = scanner.nextInt();
+        } catch (Exception ignored) {
         }
-        System.out.println(visitors.size()+" visitors: "+visitors.toString());
 
-        //2
-        LinkedHashSet<Book> books = new LinkedHashSet<>();
-        for (Person p : person){
-            books.addAll(Arrays.asList(p.getFavoriteBooks()));
-        }
-        ArrayList<String> names = new ArrayList<>();
-        for (Book b : books){
-            names.add(b.getName());
-        }
-        System.out.println(books.size()+" books: "+ names.toString());
+        switch (Select){
 
-        //3
-        Comparator<Book> YC = new YearComparator();
-        TreeSet<Book> BCbY = new TreeSet<>(YC);
-        BCbY.addAll(books);
-        System.out.println(BCbY.toString());
+            case 1:
+                ArrayList<String> visitors = new ArrayList<>();
+                for (Person p : person){
+                    visitors.add(p.getName()+" "+p.getSurname());
+                }
+                System.out.println(visitors.size()+" visitors: "+visitors);
+                break;
+            case 2:
+                LinkedHashSet<Book> books = new LinkedHashSet<>();
+                for (Person p : person){
+                    books.addAll(Arrays.asList(p.getFavoriteBooks()));
+                }
+                ArrayList<String> names = new ArrayList<>();
+                for (Book b : books){
+                    names.add(b.getName());
+                }
+                System.out.println(books.size()+" books: "+ names);
+                break;
+            case 3:
+                LinkedHashSet<Book> books1 = new LinkedHashSet<>();
+                for (Person p : person){
+                    books1.addAll(Arrays.asList(p.getFavoriteBooks()));
+                }
+                Comparator<Book> YC = new YearComparator();
+                TreeSet<Book> BCbY = new TreeSet<>(YC);
+                BCbY.addAll(books1);
+                System.out.println(BCbY);
 
-        //4
-        for (Person p : person){
-            for (Book b : p.getFavoriteBooks()){
-                if(b.getAuthor().equals("Jane Austen")) System.out.println(p.getName());
-            }
+                break;
+            case 4:
+                for (Person p : person){
+                    for (Book b : p.getFavoriteBooks()){
+                        if(b.getAuthor().equals("Jane Austen")) System.out.println(p.getName());
+                    }
+                }
+                break;
+            case 5:
+                Person readingExpert = person[0];
+                for (int i = 1; i < person.length; i++) {
+                    if (readingExpert.getFavoriteBooks().length < person[i].getFavoriteBooks().length) readingExpert = person[i];
+                }
+                System.out.println(readingExpert.getName()+" "+readingExpert.getSurname()+" readed "+readingExpert.getFavoriteBooks().length+" books");
+                break;
+            case 6:
+                ArrayList<Person> smsAgree = new ArrayList<>();
+                double mid = 0.0;
+                for (int i = 0; i < person.length; i++) {
+                    if (person[i].isSubscribed()) smsAgree.add(person[i]);
+                    mid += person[i].getFavoriteBooks().length;
+                    if(i == person.length-1) mid /= person.length;
+                }
+
+                ArrayList<SMS> smsList = new ArrayList<>();
+                for (Person p : smsAgree) {
+                    if(p.getFavoriteBooks().length > mid)  smsList.add(SMS.builder().number(p.getPhone()).message("You are bookworm").build());
+                    else if(p.getFavoriteBooks().length < mid)  smsList.add(SMS.builder().number(p.getPhone()).message("Read more").build());
+                    else smsList.add(SMS.builder().number(p.getPhone()).message("Fine").build());
+                }
+
+                System.out.println(smsList);
+
+                break;
+            default:
+                break;
+
         }
 
 
@@ -82,8 +123,11 @@ class Person{
     private String phone;
     private boolean subscribed;
     private Book[] favoriteBooks;
-    String Show(){
-        System.out.println(name);
-        return null;
-    }
+}
+
+@Data
+@Builder
+class SMS{
+    private String number;
+    private String message;
 }
